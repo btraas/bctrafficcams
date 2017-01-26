ddab824c1925a193d406202cc65408531b1fc00b
Morris' Surface <arroyomorris@outlook.com>
2017-01-25 23:24:25 -0800

    Repository as Android Stuio Project

---
+++ app/src/main/java/gitpushforce/comp3717/bcit/ca/bctrafficcams/MainActivity.java
@@ -0,0 +1,13 @@
+package gitpushforce.comp3717.bcit.ca.bctrafficcams;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
