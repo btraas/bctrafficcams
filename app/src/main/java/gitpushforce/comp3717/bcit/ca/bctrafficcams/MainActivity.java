b3fefd4410cf83845e4bb558af03a3119e7f0c3a
Brayden <btraas@gmail.com>
2017-01-26 00:07:39 -0800

    Combined main and map

--- app/src/main/java/gitpushforce/comp3717/bcit/ca/bctrafficcams/MainActivity.java
+++ app/src/main/java/gitpushforce/comp3717/bcit/ca/bctrafficcams/MainActivity.java
@@ -3,11 +3,18 @@ package gitpushforce.comp3717.bcit.ca.bctrafficcams;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.SupportMapFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
     }
 }
