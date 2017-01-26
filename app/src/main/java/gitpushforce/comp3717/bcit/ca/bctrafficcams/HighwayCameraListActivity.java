83433b388f9e96f2b5661accebdb5dd70d728585
Morris' Surface <arroyomorris@outlook.com>
2017-01-26 11:26:35 -0800

    Added Camera Classes

---
+++ app/src/main/java/gitpushforce/comp3717/bcit/ca/bctrafficcams/HighwayCameraListActivity.java
@@ -0,0 +1,44 @@
+package gitpushforce.comp3717.bcit.ca.bctrafficcams;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+
+
+public class HighwayCameraListActivity extends AppCompatActivity {
+    public static final String TAG = HighwayCameraListActivity.class.getName();
+
+    private ImageButton returnButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate begin");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_highway_camera_list);
+        returnButton = (ImageButton)findViewById(R.id.returnButton);
+        Log.d(TAG, "onCreate end");
+    }
+
+    protected  void onStart()
+    {   Log.d(TAG, "onStart begin");
+        super.onStart();
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        Log.d(TAG, "onStart end");
+    }
+    public void viewCamera(final View view)
+    {
+        Log.d(TAG, "viewCamera begin");
+        final Intent intent;
+
+        intent = new Intent(this, HighwayCameraViewActivity.class);
+        startActivity(intent);
+        Log.d(TAG, "viewCamera end");
+    }
+}
---
+++ app/src/main/java/gitpushforce/comp3717/bcit/ca/bctrafficcams/HighwayCameraViewActivity.java
@@ -0,0 +1,37 @@
+package gitpushforce.comp3717.bcit.ca.bctrafficcams;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import static gitpushforce.comp3717.bcit.ca.bctrafficcams.R.id.returnButton;
+
+public class HighwayCameraViewActivity extends AppCompatActivity {
+
+    public static final String TAG = HighwayCameraViewActivity.class.getName();
+    private ImageButton returnButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate begin");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_highway_camera_view);
+        returnButton = (ImageButton)findViewById(R.id.returnButton);
+        Log.d(TAG, "onCreate end");
+    }
+
+    @Override
+    protected void onStart()
+    {
+        Log.d(TAG, "onStart begin");
+        super.onStart();
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        Log.d(TAG, "onStart end");
+    }
+}
