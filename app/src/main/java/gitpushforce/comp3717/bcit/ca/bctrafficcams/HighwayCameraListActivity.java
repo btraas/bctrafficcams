f8573d36c55709849f582b5b58c142cb48c79f88
Morris' Surface <arroyomorris@outlook.com>
2017-02-01 20:10:48 -0800

    Made HighwayCameraListActivity an actual ListView

--- app/src/main/java/gitpushforce/comp3717/bcit/ca/bctrafficcams/HighwayCameraListActivity.java
+++ app/src/main/java/gitpushforce/comp3717/bcit/ca/bctrafficcams/HighwayCameraListActivity.java
@@ -5,41 +5,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static gitpushforce.comp3717.bcit.ca.bctrafficcams.R.id.returnButton;
 
 
-public class HighwayCameraListActivity extends AppCompatActivity {
+public class HighwayCameraListActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
     public static final String TAG = HighwayCameraListActivity.class.getName();
-
-    private ImageButton returnButton;
+    private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(TAG, "onCreate begin");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_highway_camera_list);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        returnButton = (ImageButton)findViewById(R.id.returnButton);
+        listView = (ListView)findViewById(R.id.camera_list);
+        listView.setOnItemClickListener(this);
         Log.d(TAG, "onCreate end");
     }
 
     protected  void onStart()
     {   Log.d(TAG, "onStart begin");
         super.onStart();
-        returnButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                finish();
-            }
-        });
-
         Log.d(TAG, "onStart end");
     }
-    public void viewCamera(final View view)
-    {
-        Log.d(TAG, "viewCamera begin");
-        final Intent intent;
 
+    @Override
+    public void onItemClick(AdapterView<?> adapter, final View view, int position, long id)
+    {
+        Log.d(TAG, "onItemClick begin");
+        Toast.makeText(getApplicationContext(),
+                ((TextView)view).getText(),
+                Toast.LENGTH_SHORT).show();
+        final Intent intent;
         intent = new Intent(this, HighwayCameraViewActivity.class);
         startActivity(intent);
-        Log.d(TAG, "viewCamera end");
+        Log.d(TAG, "onItemClick end");
     }
 }
